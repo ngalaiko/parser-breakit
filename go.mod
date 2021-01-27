dd89c917a94cc602955a31902b26960ff32ed586
Nikita Galaiko <nikita@galaiko.rocks>
2021-01-27 18:16:23 +0100

    parse all that's needed

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ngalaiko/parser-breakit
 
 go 1.16
 
-require github.com/PuerkitoBio/goquery v1.6.1 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.6.1
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
+)
--- parser.go
+++ parser.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
 // Parser parses articles from https://breakit.se
@@ -30,8 +31,8 @@ type Article struct {
 	URL         *url.URL
 	PublishedAt time.Time
 	Title       string
-	Subtitle    string
-	Summary     string
+	Preamble    string
+	Summary     *string
 }
 
 // Parse starts parsing.
@@ -47,6 +48,8 @@ func (p *Parser) Parse(ctx context.Context, depth int64, concurrency int64) ([]*
 
 	return articles, nil
 }
+
+var loc, _ = time.LoadLocation("Europe/Stockholm")
 
 func (p *Parser) parse(ctx context.Context, depth int64, url *url.URL) ([]*Article, error) {
 	if depth < 0 {
@@ -93,38 +96,64 @@ func (p *Parser) parsePage(ctx context.Context, link *url.URL) (*Article, []*url
 }
 
 func extractContent(src *url.URL, doc *goquery.Document) *Article {
-	return &Article{
-		URL: src,
+	a := &Article{
+		URL:      src,
+		Title:    doc.Find(".article__title").Text(),
+		Preamble: doc.Find(".article__preamble").Text(),
 	}
+
+	if s := doc.Find(".article__body").First().First().Text(); s != "" {
+		a.Summary = &s
+	}
+
+	if date := doc.Find(".article__date"); date != nil && len(date.Nodes) != 0 {
+		if datetime := getAttribute(date.Nodes[0], "datetime"); datetime != nil {
+			if publishedAt, err := time.Parse("2006-01-02 15:04:05", *datetime); err == nil {
+				a.PublishedAt = publishedAt.In(loc)
+			}
+		}
+	}
+
+	return a
 }
 
 func extractLinks(src *url.URL, doc *goquery.Document) []*url.URL {
 	links := []*url.URL{}
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
 		for _, n := range s.Nodes {
-			for _, attribute := range n.Attr {
-				if attribute.Key != "href" {
-					continue
-				}
-
-				link, err := url.Parse(attribute.Val)
-				if err != nil {
-					continue
-				}
-
-				if !isArticle(link) {
-					continue
-				}
-
-				link.Scheme = src.Scheme
-				link.Host = src.Host
-
-				links = append(links, link)
+			href := getAttribute(n, "href")
+			if href == nil {
+				continue
 			}
+
+			link, err := url.Parse(*href)
+			if err != nil {
+				continue
+			}
+
+			if !isArticle(link) {
+				continue
+			}
+
+			link.Scheme = src.Scheme
+			link.Host = src.Host
+
+			links = append(links, link)
 		}
 	})
 
 	return links
+}
+
+func getAttribute(node *html.Node, name string) *string {
+	for _, attribute := range node.Attr {
+		if attribute.Key != name {
+			continue
+		}
+		return &attribute.Val
+	}
+
+	return nil
 }
 
 func isArticle(link *url.URL) bool {
